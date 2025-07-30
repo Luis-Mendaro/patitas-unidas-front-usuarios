4f1317047766535b736ca88f594d9b93727c13b3
JP Maronna <jpmaronna@gmail.com>
2025-07-30 13:10:34 -0300

    config del repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
 import "./App.css";
+
+import { Route, Routes } from "react-router";
+import { ToastContainer } from "react-toastify";
+
+import Home from "./pages/Home";
+import PublicRoutes from "./routes/PublicRoutes";
 
 function App() {
   return (
     <>
-      <h1>HOME</h1>
+      <Routes>
+        <Route element={<PublicRoutes />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+
+      <ToastContainer />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,16 @@
-import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router";
+
+import App from "./App.jsx";
+import store from "./config/redux/storeConfig.js";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
