10192280eb7123042760ae28dbf8b2cf592bf27c
Luis-Mendaro <securedev.lm@gmail.com>
2025-07-31 09:30:54 -0300

    Add Adoption Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
+import "./pages/AdoptionForm.css";
 
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import Home from "./pages/Home";
+import AdoptionForm from "./pages/AdoptionForm";
 import PublicRoutes from "./routes/PublicRoutes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 
 function App() {
   return (
@@ -15,6 +18,7 @@ function App() {
       <Routes>
         <Route element={<PublicRoutes />}>
           <Route path="/" element={<Home />} />
+          <Route path="/adopcion" element={<AdoptionForm />} />
         </Route>
       </Routes>
       <Footer />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <>
-      <div className="w-100 bg-black" style={{ height: "50px" }}></div>
-    </>
+    <div className="w-100 bg-black d-flex align-items-center justify-content-end px-3" style={{ height: "50px" }}>
+      <Link to="/adopcion" className="btn btn-primary">
+        Adoptar
+      </Link>
+    </div>
   );
 }
 
