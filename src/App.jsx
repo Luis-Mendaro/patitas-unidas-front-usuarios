d56f07a5539f3264c1f5b47e3e01ed9055fa82ac
Luis-Mendaro <securedev.lm@gmail.com>
2025-07-31 09:56:32 -0300

    changes navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import "./pages/AdoptionForm.css";
 
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router";
 import { ToastContainer } from "react-toastify";
 
 import Home from "./pages/Home";
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,8 @@
-import { Link } from "react-router-dom";
-
 function Navbar() {
   return (
-    <div className="w-100 bg-black d-flex align-items-center justify-content-end px-3" style={{ height: "50px" }}>
-      <Link to="/adopcion" className="btn btn-primary">
-        Adoptar
-      </Link>
-    </div>
+    <>
+      <div className="w-100 bg-black" style={{ height: "50px" }}></div>
+    </>
   );
 }
 
