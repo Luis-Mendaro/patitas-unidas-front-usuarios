be83e74ebe4c5647f77a44cbc66e39af34fa19fe
JP Maronna <105681949+juanpimaro@users.noreply.github.com>
2025-07-31 09:32:27 -0300

    PetDetail y paw.svg (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import PublicRoutes from "./routes/PublicRoutes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import PetDetail from "./pages/PetDetail";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route element={<PublicRoutes />}>
           <Route path="/" element={<Home />} />
+          <Route path="/petDetail" element={<PetDetail />} />
         </Route>
       </Routes>
       <Footer />
