20176929300e06ba59fcdd492d2e8186b15135f4
joaquinpachon <joaquinpachon1997@gmail.com>
2025-08-07 09:58:29 -0300

    profile

--- src/pages/profileUser.jsx
+++ src/pages/profileUser.jsx
@@ -1,33 +1,64 @@
-import React from "react";
 import "./ProfileUser.css";
-
-const profileData = {
-    name: "Nombre",
-    email: "",
-    role: "",
-    location: "",
-    phone: "",
-    joined: "",
-    avatar: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
-    likedPets: [],
-
-};
+import { useState } from "react";
 
 const ProfileUser = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+
     return (
         <div className="profile-container">
-            <div className="profile-card">
-                <img src={profileData.avatar} alt="Avatar" className="avatar" />
-                <h2>{profileData.name}</h2>
-                <hr />
-                <p><strong>Email:</strong> {profileData.email}</p>
-                <p><strong>miembro desde:</strong> {new Date(profileData.joined).toLocaleDateString()}</p>
-                <ul>
-                    {profileData.likedPets.map((pet, index) => (
-                        <li key={index}>{pet}</li>
-                    ))}
-                </ul>
+            <div className="header">
+                <button type="button" className="edit-btn">Editar</button>
             </div>
+
+            <form className="profile-form" onSubmit={handleSubmit}>
+                <div className="avatar">
+                    <div className="avatar-circle">
+                        <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="Avatar por defecto" className="avatar-img" />
+                    </div>
+                </div>
+
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Nombre"
+                    value={formData.name}
+                    onChange={handleChange}
+                />
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Contraseña"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+
+                <div className="guardar-container">
+                    <button type="submit" className="save-btn">Guardar</button>
+                </div>
+            </form>
         </div>
     );
 };
