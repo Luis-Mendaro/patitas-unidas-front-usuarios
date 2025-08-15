1d127fbe75c6b3ec04b79014a5d6abc532964dbd
nikof7 <40549719+nikof7@users.noreply.github.com>
2025-08-15 12:18:25 -0300

    Shelter detail now is dynamic

--- src/pages/ShelterDetail.jsx
+++ src/pages/ShelterDetail.jsx
@@ -1,101 +1,115 @@
-import { useNavigate, useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import Button from "../components/Button";
 import BackButton from "../components/BackButton";
+import { useEffect } from "react";
+import { useApi } from "../hooks/useApi";
+import { useState } from "react";
+import PetCard from "../components/PetCard";
 
 function ShelterDetail() {
-  // Con esto se puede hacer la llamada y obtener todos los datos del refugio.
   const { id } = useParams();
-  const navigate = useNavigate();
+  const navigate = useNavigate()
+  const { getShelterById } = useApi()
+  const [shelter, setShelter] = useState(false)
 
-  const shelter = {
-    id: 1,
-    name: "Animales sin hogar",
-    creationDate: "2009",
-    location: "Montevideo, Uruguay",
-    description:
-      "Somos una organización sin fines de lucro dedicada al rescate, rehabilitación y reubicación de animales abandonados. Trabajamos incansablemente para brindar una segunda oportunidad a aquellos que más lo necesitan, proporcionando atención médica, amor y cuidado hasta encontrarles un hogar permanente. Cada día enfrentamos historias difíciles, pero también somos testigos de transformaciones increíbles. Detrás de cada rescate hay un equipo comprometido, voluntarios apasionados y una comunidad que cree en el valor de la compasión. Nos enfocamos no solo en salvar vidas, sino también en crear conciencia sobre la tenencia responsable, el respeto hacia todos los seres vivos y la importancia de la adopción como primer camino. Gracias al apoyo de personas solidarias, podemos seguir ofreciendo alimento, tratamientos veterinarios y un refugio temporal a decenas de animales que esperan por una familia. Creemos que cada vida importa y que, con empatía y acción colectiva, podemos construir un mundo más justo para ellos.",
-    rating: 4.8,
-    totalReviews: 127,
-    images: [
-      "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
-      "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
-      "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
-    ],
-    socialMedia: {
-      instagram: "@animalessinhogar",
-      facebook: "AnimalesSinHogar",
-      twitter: "@animales_hogar",
-    },
-    contact: {
-      phone: "+34 600 123 456",
-      email: "contacto@animalessinhogar.org",
-    },
-    animals: [
-      {
-        id: 1,
-        name: "Luna",
-        image:
-          "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
-      },
-      {
-        id: 2,
-        name: "Max",
-        image:
-          "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
-      },
-      {
-        id: 3,
-        name: "Bella",
-        image:
-          "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
-      },
-      {
-        id: 4,
-        name: "Rocky",
-        image:
-          "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
-      },
-      {
-        id: 5,
-        name: "Mia",
-        image:
-          "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
-      },
-      {
-        id: 6,
-        name: "Charlie",
-        image:
-          "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
-      },
-    ],
-    products: [
-      {
-        id: 1,
-        name: "Comida",
-      },
-      {
-        id: 2,
-        name: "Juguetes",
-      },
-      {
-        id: 3,
-        name: "Camas",
-      },
-      {
-        id: 4,
-        name: "Medicamentos",
-      },
-      {
-        id: 5,
-        name: "Collares",
-      },
-      {
-        id: 6,
-        name: "Juguetes",
-      },
-    ],
-  };
-  return (
+  // const shelter = {
+  //   id: 1,
+  //   name: "Animales sin hogar",
+  //   creationDate: "2009",
+  //   location: "Montevideo, Uruguay",
+  //   description:
+  //     "Somos una organización sin fines de lucro dedicada al rescate, rehabilitación y reubicación de animales abandonados. Trabajamos incansablemente para brindar una segunda oportunidad a aquellos que más lo necesitan, proporcionando atención médica, amor y cuidado hasta encontrarles un hogar permanente. Cada día enfrentamos historias difíciles, pero también somos testigos de transformaciones increíbles. Detrás de cada rescate hay un equipo comprometido, voluntarios apasionados y una comunidad que cree en el valor de la compasión. Nos enfocamos no solo en salvar vidas, sino también en crear conciencia sobre la tenencia responsable, el respeto hacia todos los seres vivos y la importancia de la adopción como primer camino. Gracias al apoyo de personas solidarias, podemos seguir ofreciendo alimento, tratamientos veterinarios y un refugio temporal a decenas de animales que esperan por una familia. Creemos que cada vida importa y que, con empatía y acción colectiva, podemos construir un mundo más justo para ellos.",
+  //   rating: 4.8,
+  //   totalReviews: 127,
+  //   images: [
+  //     "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
+  //     "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
+  //     "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
+  //   ],
+  //   socialMedia: {
+  //     instagram: "@animalessinhogar",
+  //     facebook: "AnimalesSinHogar",
+  //     twitter: "@animales_hogar",
+  //   },
+  //   contact: {
+  //     phone: "+34 600 123 456",
+  //     email: "contacto@animalessinhogar.org",
+  //   },
+  //   animals: [
+  //     {
+  //       id: 1,
+  //       name: "Luna",
+  //       image:
+  //         "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "Max",
+  //       image:
+  //         "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "Bella",
+  //       image:
+  //         "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "Rocky",
+  //       image:
+  //         "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
+  //     },
+  //     {
+  //       id: 5,
+  //       name: "Mia",
+  //       image:
+  //         "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
+  //     },
+  //     {
+  //       id: 6,
+  //       name: "Charlie",
+  //       image:
+  //         "https://www.blueridgehumane.org/wp-content/uploads/2019/06/dog-2243682_1920.jpg",
+  //     },
+  //   ],
+  //   products: [
+  //     {
+  //       id: 1,
+  //       name: "Comida",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "Juguetes",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "Camas",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "Medicamentos",
+  //     },
+  //     {
+  //       id: 5,
+  //       name: "Collares",
+  //     },
+  //     {
+  //       id: 6,
+  //       name: "Juguetes",
+  //     },
+  //   ],
+  // };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getShelterById(id)
+      setShelter(response.data.shelter)
+    }
+    fetchData()
+  }, [id])
+
+  return shelter && (
     <main className="container">
       <BackButton />
 
@@ -111,7 +125,7 @@ function ShelterDetail() {
             <div className="position-absolute top-0 start-0 w-100 h-100" />
           </div>
 
-          <div className="row g-3">
+          {/* <div className="row g-3">
             {shelter.images.slice(1).map((image, index) => (
               <div key={index} className="col-6">
                 <div className="rounded overflow-hidden">
@@ -124,7 +138,7 @@ function ShelterDetail() {
                 </div>
               </div>
             ))}
-          </div>
+          </div> */}
         </div>
 
         <div className="col-lg-6">
@@ -175,57 +189,22 @@ function ShelterDetail() {
       </div>
 
       <section className="mt-5">
-        <div className="d-flex justify-content-between align-items-center mb-4">
-          <h2 className="h4 fw-bold">Nuestros Animales</h2>
-          <Button variant="secondary" text="Ver todos" />
-        </div>
         <div className="row g-3">
-          {shelter.animals.map((animal) => (
-            <div key={animal.id} className="col-6 col-md-4 col-lg-2">
-              <div
-                className="position-relative rounded overflow-hidden"
-                style={{ aspectRatio: "1 / 1", cursor: "pointer" }}
-              >
-                <img
-                  src={animal.image}
-                  alt={animal.name}
-                  className="w-100 h-100"
-                  style={{
-                    objectFit: "cover",
-                    transition: "transform 0.3s",
-                  }}
-                />
-                <div
-                  className="position-absolute top-0 start-0 w-100 h-100"
-                  style={{
-                    background:
-                      "linear-gradient(to top, rgba(0,0,0,0.4), transparent)",
-                    opacity: 0,
-                    transition: "opacity 0.3s",
-                  }}
-                />
-                <div
-                  className="position-absolute bottom-0 start-0 text-white fw-medium px-2 pb-2"
-                  style={{
-                    opacity: 0,
-                    transition: "opacity 0.3s",
-                  }}
-                >
-                  {animal.name}
-                </div>
-              </div>
+          {shelter.pets.map((animal) => (
+            <div key={animal.id} className="col-12 col-md-6 col-lg-4 col-xl-3">
+              <PetCard pet={animal} key={animal.id} />
             </div>
           ))}
         </div>
       </section>
 
-      <section className="mt-5">
-        <div className="d-flex justify-content-between align-items-center mb-4">
+      <section className="my-5">
+        {/* <div className="d-flex justify-content-between align-items-center mb-4">
           <h2 className="h4 fw-bold">Productos</h2>
-          <Button variant="secondary" text="Ver todos" />
-        </div>
+          <Link to="/mascotas"><Button variant="secondary" text="Ver todos" /></Link>
+        </div> */}
         <div className="row g-3">
-          {shelter.products.map((product) => (
+          {shelter.products.slice(0, 5).map((product) => (
             <div key={product.id} className="col-6 col-md-4 col-lg-2">
               <div
                 className="bg-light rounded d-flex align-items-center justify-content-center"
@@ -239,6 +218,22 @@ function ShelterDetail() {
               </div>
             </div>
           ))}
+
+          <div className="col-6 col-md-4 col-lg-2">
+            <Link to="/">
+              <div
+                className="bg-light rounded d-flex align-items-center justify-content-center"
+                style={{ aspectRatio: "1 / 1" }}
+              >
+                <div className="text-center p-3">
+                  <p className="text-dark small fw-medium mb-0">
+                    Ver más.
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </div>
+
         </div>
       </section>
     </main>
