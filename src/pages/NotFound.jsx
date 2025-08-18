d10458391f087912b0f6df30b12d27437a01a002
nikof7 <40549719+nikof7@users.noreply.github.com>
2025-08-17 21:14:14 -0300

    idea nico

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import "./NotFound.css"; // ← Crear este archivo CSS
 
 function NotFound() {
   return (
-    <div className="container text-center mt-5 mb-5 not-found-container">
+    <div className="d-flex flex-column justify-content-center container text-center my-5 not-found-container">
       <div className="row justify-content-center">
         <div className="col-md-6">
           {/* Logo animado */}
@@ -18,20 +18,10 @@ function NotFound() {
 
           <h1 className="display-1 fw-bold text-muted mb-3">404</h1>
           <h2 className="mb-4">¡Ups! Esta página se perdió</h2>
-          <p className="lead text-muted mb-4">
-            Como un cachorro travieso, se escapó y no podemos encontrarla.
-          </p>
-
-          <Link to="/" className="patas-btn patas-btn-primary patas-btn-lg">
-            ¡Veni, no te vayas! <span> </span> Volve a casa
-          </Link>
 
           <div className="mt-4">
-            <Link to="/mascotas" className="patas-btn patas-btn-secondary me-2">
-              Ver Mascotas
-            </Link>
-            <Link to="/refugios" className="patas-btn patas-btn-secondary">
-              Ver Refugios
+            <Link to="/" className="patas-btn patas-btn-secondary me-2">
+              Ir al inicio
             </Link>
           </div>
         </div>
